8848309bae558fddce00f99e13bfc81bf18b334b
HoangNN <ken@nustechnology.com>
2019-07-23 17:09:23 +0700

    added visit as guest

--- Fotobook/config/application.rb
+++ Fotobook/config/application.rb
@@ -12,6 +12,14 @@ module Fotobook
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("app", "assets", "uploads")
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"
+      Devise::PasswordsController.layout "devise"
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
